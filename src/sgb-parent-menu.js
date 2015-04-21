d1a956f7bc0286e8707b3f2a1c82699ed694ab7d
aaramirez <alexander.ramirez@synergy-gb.com>
2015-04-20 20:35:01 -0430

    Controller added and template

---
+++ src/sgb-parent-menu.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('sgb-parent-menu', ['megazord'])
+    .controller('sgb-parent-menu-controller', ['$scope', '_router', '_screenParams', function($scope, _router, _screenParams){
+
+        $scope.menu = _screenParams.menu;
+        $scope.navBarTitle = _screenParams.title?_screenParams.title:'default_menu_title';
+        $scope.navBarColor = _screenParams.barColor?_screenParams.barColor:'defaultBarColor';
+        $scope.navBarSide = _screenParams.side?_screenParams.side:'right';
+        $scope.navBarHeaderColor = _screenParams.headerColor?_screenParams.headerColor:'defaultHeaderColor';
+
+        $scope.gotoScreen = function(screen){
+            _router.gotoState(screen);
+        };
+    }]);

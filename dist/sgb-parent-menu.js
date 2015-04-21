61a7c154ce935a3ef4b7266510807a659a764b0f
aaramirez <alexander.ramirez@synergy-gb.com>
2015-04-20 21:04:00 -0430

    Dist generation

--- dist/sgb-parent-menu.js
+++ dist/sgb-parent-menu.js
@@ -1,3 +1,17 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+'use strict';
 
+angular.module('sgb-parent-menu', ['megazord'])
+    .controller('sgb-parent-menu-controller', ['$scope', '_router', '_screenParams', function($scope, _router, _screenParams){
+
+        $scope.menu = _screenParams.menu?_screenParams.menu:{};
+        $scope.navBarTitle = _screenParams.title?_screenParams.title:'default_menu_title';
+        $scope.navBarColor = _screenParams.barColor?_screenParams.barColor:'defaultBarColor';
+        $scope.navBarSide = _screenParams.side?_screenParams.side:'right';
+        $scope.navBarHeaderColor = _screenParams.headerColor?_screenParams.headerColor:'defaultHeaderColor';
+        $scope.backViews = _screenParams.backview?_screenParams.backview:'true';
+        $scope.gotoScreen = function(screen){
+            _router.gotoState(screen);
+        };
+    }]);
 },{}]},{},[1]);

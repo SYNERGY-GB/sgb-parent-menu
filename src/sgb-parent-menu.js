4916e74285c179c0f88c3e55d96258c6a073d155
aaramirez <alexander.ramirez@synergy-gb.com>
2015-04-20 20:56:53 -0430

    Param for backview added

--- src/sgb-parent-menu.js
+++ src/sgb-parent-menu.js
@@ -3,7 +3,7 @@
 angular.module('sgb-parent-menu', ['megazord'])
     .controller('sgb-parent-menu-controller', ['$scope', '_router', '_screenParams', function($scope, _router, _screenParams){
 
-        $scope.menu = _screenParams.menu;
+        $scope.menu = _screenParams.menu?_screenParams.menu:{};
         $scope.navBarTitle = _screenParams.title?_screenParams.title:'default_menu_title';
         $scope.navBarColor = _screenParams.barColor?_screenParams.barColor:'defaultBarColor';
         $scope.navBarSide = _screenParams.side?_screenParams.side:'right';

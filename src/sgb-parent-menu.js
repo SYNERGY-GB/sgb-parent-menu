3094430595eea633ce22067c83edc90e45c23aad
Luis Miglietti <luismiglietti@gmail.com>
2015-12-01 16:46:07 -0430

    added optional service call

--- src/sgb-parent-menu.js
+++ src/sgb-parent-menu.js
@@ -1,7 +1,8 @@
 'use strict';
 
 angular.module('sgb-parent-menu', ['megazord'])
-    .controller('sgb-parent-menu-controller', ['$scope', '$rootScope', '$ionicLoading', '$translate', '_router', '_screenParams', function ($scope, $rootScope, $ionicLoading, $translate, _router, _screenParams) {
+    .controller('sgb-parent-menu-controller', ['$scope', '$rootScope', '$ionicLoading', '$translate', '_router', '_screenParams','$injector','$http',
+        function ($scope, $rootScope, $ionicLoading, $translate, _router, _screenParams, $injector, $http) {
 
         $rootScope.$on('_dataLoadStarted', function () {
             console.log('Should start spinner.');
@@ -21,7 +22,20 @@ angular.module('sgb-parent-menu', ['megazord'])
         $scope.navBarSide = _screenParams.side?_screenParams.side:'right';
         $scope.navBarHeaderColor = _screenParams.headerColor?_screenParams.headerColor:'defaultHeaderColor';
         $scope.backViews = _screenParams.backview?_screenParams.backview:'true';
-        $scope.gotoScreen = function(screen){
-            _router.goToState(screen);
+        $scope.gotoScreen = function(screen, url){
+            if (url) {
+                var loader; 
+                if ($injector.has('$appLoader')) loader = $injector.get('$appLoader');
+                if (loader) loader.show(); 
+                $http.get(url)
+                    .then(function(result) {
+                        if (loader) loader.hide(); 
+                        _router.goToState(screen, result.data);
+                    }).catch(function(result) {
+                        if (loader) loader.hide(); 
+                    })
+            } else {
+                _router.goToState(screen);
+            }
         };
     }]);

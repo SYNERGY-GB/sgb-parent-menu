a5114f090ee41f5649b825077d6b58af29f2e715
Jonathan Duarte <jonathan.duarte@synergy-gb.com>
2015-12-10 16:54:01 -0430

    Cambios en el Parent Menu

--- dist/sgb-parent-menu.js
+++ dist/sgb-parent-menu.js
@@ -2,7 +2,10 @@
 'use strict';
 
 angular.module('sgb-parent-menu', ['megazord'])
-    .controller('sgb-parent-menu-controller', ['$scope', '$rootScope', '$ionicLoading', '$translate', '_router', '_screenParams', function ($scope, $rootScope, $ionicLoading, $translate, _router, _screenParams) {
+    .controller('sgb-parent-menu-controller', ['$scope', '$rootScope', '$ionicLoading', '$translate', '_router', '_screenParams', '_screen', function ($scope, $rootScope, $ionicLoading, $translate, _router, _screenParams, _screen) {
+
+        _screen.initialize($scope, _screenParams);
+        $scope.params =_screenParams;
 
         $rootScope.$on('_dataLoadStarted', function () {
             console.log('Should start spinner.');
@@ -22,8 +25,41 @@ angular.module('sgb-parent-menu', ['megazord'])
         $scope.navBarSide = _screenParams.side?_screenParams.side:'right';
         $scope.navBarHeaderColor = _screenParams.headerColor?_screenParams.headerColor:'defaultHeaderColor';
         $scope.backViews = _screenParams.backview?_screenParams.backview:'true';
+    
         $scope.gotoScreen = function(screen){
             _router.goToState(screen);
         };
-    }]);
+        
+        
+        if(_screenParams.selItem === true) {
+                  
+           $scope.menu[0].selection=true;
+          $scope.selMenuItem = function(opcion) {
+            var i;
+            for(i=0;i<$scope.menu.length;i++) {
+            if ($scope.menu[i].screen==opcion) {
+              $scope.menu[i].selection = true;
+            break;
+          } 
+        }
+      };
+  
+        $scope.unselMenuItem = function(opcion) {
+            var i;
+            for(i=0;i<$scope.menu.length;i++) {
+            if ($scope.menu[i].screen==opcion) {
+                $scope.menu[i].selection = false;
+            break;
+          }
+        }
+      };
+    $scope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams) {
+    $scope.unselMenuItem(fromState.name);
+    $scope.selMenuItem(toState.name);
+  }); 
+
+
+};
+}]);
+
 },{}]},{},[1]);
---
+++ fixtures/config.js
@@ -0,0 +1,8 @@
+//Screen sample configuration (for development purposes)
+config = {
+	menu: [
+                {title: 'Planes', screen: 'Planes',icon:"icon ion-iphone",selection:false},
+                {title: 'Servicios', screen: 'Servicios',icon:"icon ion-android-laptop",selection:false}
+          ],
+    "selItem":true           
+  };
